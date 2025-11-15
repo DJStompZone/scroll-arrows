ae704e3d4511d96274db307b33b0365f2806c6f9
DJ Stomp <85457381+DJStompZone@users.noreply.github.com>
2025-11-15 01:34:21 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+$(function() {
+  var bgImages = [
+    'https://picsum.photos/1366/768?random=1',
+    'https://picsum.photos/1366/768?random=2'
+  ];
+
+  function preload(src) {
+    return new Promise(function(resolve) {
+      var img = new Image();
+      img.onload = resolve;
+      img.onerror = resolve;
+      img.crossOrigin = 'anonymous';
+      img.src = src;
+    });
+  }
+
+  var reveal = function() {
+    $('html, body').css({ filter: 'none', opacity: 1 });
+    $('body').css('overflow', 'scroll');
+  };
+
+  if (window.Promise) {
+    Promise.all(bgImages.map(preload)).then(reveal);
+    $(window).on('load', reveal);
+  } else {
+    $(window).on('load', reveal);
+  }
+
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
